072c42f9fd511f2e47d7786052d54846833f3ac2
harcolemas@gmail.com <harcolemas@gmail.com>
2023-11-12 17:44:19 +0500

    Centered printing for matrix
    Works well for all single digits matrix but becomes less centered form larger ones

--- src/ui.c
+++ src/ui.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
-#include <stdlib.h>
 // #include <windows.h>
 
 // Linux
-int getWidthLinux(){
+int getWidthLinux() {
   struct winsize w;
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 
   return w.ws_col;
 }
 
-int getHeightLinux(){
+int getHeightLinux() {
   struct winsize w;
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 
@@ -21,102 +21,149 @@ int getHeightLinux(){
 }
 // Windows
 // int getWidthWin(){
-   // CONSOLE_SCREEN_BUFFER_INFO csbi;
-    // GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+// CONSOLE_SCREEN_BUFFER_INFO csbi;
+// GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
 //
-    // int consoleWidth = csbi.dwSize.X;
-    // return consoleWidth;
+// int consoleWidth = csbi.dwSize.X;
+// return consoleWidth;
 // }
 
-
 void printCenteredText(const char *text, int totalWidth) {
-    const char *delim = "\n";  // Split text into lines by newline character
-    char *token, *next;
-    char *textCopy = strdup(text);  // Make a copy to avoid modifying the original
-
-    token = strtok_r(textCopy, delim, &next);
-    while (token != NULL) {
-        int textLength = strlen(token);
-        if (textLength >= totalWidth) {
-            // Text is longer than the available width, just print it as is
-            printf("%s\n", token);
-        } else {
-            int padding = (totalWidth - textLength) / 2;
-
-            // Print leading spaces
-            for (int i = 0; i < padding; i++) {
-                printf(" ");
-            }
-
-            // Print the text
-            printf("%s\n", token);
-        }
-        token = strtok_r(NULL, delim, &next);
+  // Vertical Centering
+  int cnt = 0;
+  for (int i = 0; i < strlen(text); i++) {
+    if (text[i] == '\n') {
+      cnt++;
     }
+  }
+  int vPads = (getHeightLinux() - cnt) / 2;
+  // printf("Vertical Padding = %d \nLines= %d\nHeight = %d", vPads, cnt,
+  // getHeightLinux());
+  for (int i = 0; i < vPads; i++) {
+    printf("\n");
+  }
 
-    free(textCopy);  // Free the memory allocated by strdup
+  const char *delim = "\n"; // Split text into lines by newline character
+  char *token, *next;
+  char *textCopy = strdup(text); // Make a copy to avoid modifying the original
+
+  token = strtok_r(textCopy, delim, &next);
+  while (token != NULL) {
+    int textLength = strlen(token);
+    if (textLength >= totalWidth) {
+      // Text is longer than the available width, just print it as is
+      printf("%s\n", token);
+    } else {
+      int padding = (totalWidth - textLength) / 2;
+
+      // Print leading spaces
+      for (int i = 0; i < padding; i++) {
+        printf(" ");
+      }
+
+      // Print the text
+      printf("%s\n", token);
+    }
+    token = strtok_r(NULL, delim, &next);
+  }
+
+  free(textCopy); // Free the memory allocated by strdup
 }
 
-
-void mainM()
-{
-  const char *menu = "1.Arithmetic\n2.Quadratic&Cubic\n3.Logarithmic\n4.Trigonometric\n5.Matrices\n6.Exit\n";
+void mainM() {
+  const char *menu = "1.Arithmetic\n2.Quadratic&Cubic\n3.Logarithmic\n4."
+                     "Trigonometric\n5.Matrices\n6.Exit\n";
   int width = getWidthLinux();
   printCenteredText(menu, width);
   int n;
   scanf("%d", &n);
   switch (n) {
-    case 1:
-      // Arithmetic Menu
-      break;
-    case 2:
-      // Quadratic
-      break;
-    case 3:
-      // Logarithmic
-      break;
-    case 4:
-      // Trignometric
-      break;
-    case 5:
-      // Matrices
-      break;
-    case 6:
-    default:
-      // Exit
-      printf("Exiting...\n");
-      break;
+  case 1:
+    // Arithmetic Menu
+    break;
+  case 2:
+    // Quadratic
+    break;
+  case 3:
+    // Logarithmic
+    break;
+  case 4:
+    // Trignometric
+    break;
+  case 5:
+    // Matrices
+    break;
+  case 6:
+  default:
+    // Exit
+    printf("Exiting...\n");
+    break;
   }
-
 }
 
-
-void printMatr(int r, int c, int mat[3][3], int px, int py){
+void printMatr(int r, int c, int mat[4][4], int px, int py) {
+  int size = r*c*4 + 1;
+  char matrix[size];
+  matrix[0]='\0';
 
   for (int i = 0; i < r; i++) {
     for (int j = 0; j < c; j++) {
-      if (i == px && j == py){
-        printf("[%d] ", mat[i][j]);
-      } else if (j == 0){
-      printf("[%d ", mat[i][j]);
-      } else if(j == (c-1)){
-      printf("%d]\n", mat[i][j]);
+      if (i == px && j == py) {
+        // printf("[%d] ", mat[i][j]);
+        char part[5] = "[";
+        char num[10000];
+        sprintf(num, "%d", mat[i][j]);
+        strcat(part, num);
+        strcat(part, "]");
+        strcat(matrix, part);
+      } else if (j == 0) {
+        // printf("[%d ", mat[i][j]);
+        char part[5] = "[";
+        char num[10000];
+        sprintf(num, "%d", mat[i][j]);
+        strcat(part, num);
+        strcat(part, " ");
+        strcat(matrix, part);
+      } else if (j == (c - 1)) {
+        // printf("%d]\n", mat[i][j]);
+        char part[5] = " ";
+        char num[10000];
+        sprintf(num, "%d", mat[i][j]);
+        strcat(part, num);
+        strcat(part, "]");
+        strcat(matrix, part);
+        strcat(matrix, "\n");
       } else {
-      printf("_ ");
+        // printf("_ ");
+        char part[3] = " _ ";
+        strcat(matrix, part); 
       }
+      // printf("Working?\n");
     }
   }
+  // puts(matrix);
+  printCenteredText(matrix, getWidthLinux()-1);
 }
 
-int main(int argc, char const* argv[])
-{
-  int mat[3][3] = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
-  printMatr(3,3, mat, 1,1);
+int main(int argc, char const *argv[]) {
+  int mat[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+int mat2x2[2][2] = {
+  {1, 2},
+  {3, 4}
+};
+  printf("The Matrix calculator\n");
+  // system("clear");
+  int mat4x4[4][4] = {
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 1, 1, 7},
+    {1, 3, 5, 6}
+  };
+  // printf("\n");
+  // printMatr(3,3, mat, 1,1);
+  printf("\n");
+  printMatr(4,4, mat4x4, 1,1);
   // mainM();
+  // printf("%d\n", getHeightLinux());
   return 0;
 }
-
