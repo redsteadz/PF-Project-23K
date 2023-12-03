fc35f217c0a36a3c19f0bc4a189b3428fc4a688e
harcolemas@gmail.com <harcolemas@gmail.com>
2023-12-03 21:17:24 +0500

    finalise + 1

--- src/single.c
+++ src/single.c
@@ -1,3 +1,224 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef _WIN32
+#include <windows.h>
+#include <conio.h>
+// Windows
+int getWidth() {
+  // CONSOLE_SCREEN_BUFFER_INFO is a Struct that stores info abt.. the screen 0_0
+  CONSOLE_SCREEN_BUFFER_INFO csbi; // Makes a struct named csbi
+  GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi); // Assigns it values of the console
+  
+  // Now that it is assigned, It can be accessed using this
+  int consoleWidth = csbi.dwSize.X;
+  return consoleWidth;
+}
+int getHeight() {
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+  // Same thing as before, Except We find the Lines from Bottom to top
+  int consoleHeight = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+  return consoleHeight;
+}
+void clearScreen(){
+	system("cls");
+}
+
+#else
+
+// Aap log linux istemal kro to smjhaunga
+
+#include <sys/ioctl.h>
+#include <unistd.h>
+// Linux
+int getWidth() {
+  struct winsize w;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+  return w.ws_col;
+}
+
+int getHeight() {
+  struct winsize w;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+  return w.ws_row;
+}
+void clearScreen(){
+	system("clear");
+}
+#endif
+
+
+void printCenteredText(const char *text) {
+  clearScreen();
+  int totalWidth = getWidth();
+  // Vertical Centering
+  int cnt = 0;
+  for (int i = 0; i < strlen(text); i++) {
+    if (text[i] == '\n') {
+      cnt++;
+    }
+  }
+  int vPads = (getHeight() - cnt) / 2;
+
+  for (int i = 0; i < vPads; i++) {
+    printf("\n");
+  }
+
+  const char *delim = "\n"; // Split text into lines by newline character
+  char *token, *next;
+  char *textCopy = strdup(text); // Make a copy to avoid modifying the original
+
+  token = strtok_r(textCopy, delim, &next);
+  while (token != NULL) {
+
+    int textLength = strlen(token);
+    if (textLength >= totalWidth) {
+      // Text is longer than the available width, just print it as is
+      printf("%s\n", token);
+    } else {
+      int padding = (totalWidth - textLength) / 2;
+
+      // Print leading spaces
+      for (int i = 0; i < padding; i++) {
+        printf(" ");
+      }
+
+      // Print the text
+      printf("%s\n", token);
+
+    }
+    token = strtok_r(NULL, delim, &next);
+  }
+
+  free(textCopy); // Free the memory allocated by strdup
+}
+
+
+// Update Menu
+
+void updateMenu(char arr[][100], int size, int pos) {
+  char buffer[1000]="";
+  for (int i = 0; i < size; i++) {
+    char part[100]="";
+    if (i != pos) {
+      sprintf(part, "%s\n", arr[i]);
+    } else {
+      sprintf(part, "[[ %s ]]\n", arr[i]);
+    }
+    strncat(buffer, part, strlen(part));
+  }
+  printCenteredText(buffer);
+}
+
+// Select Menu
+
+int selectMenu(char arr[][100], int size) {
+    #ifdef _WIN32
+    #else
+    system("stty -icanon");
+    #endif
+
+    clearScreen();
+    updateMenu(arr, size, 0);
+
+    char ch = 0;
+    int pos = 0;
+    
+    // Windows Specific, _getch() is used to get the input
+    #ifdef _WIN32
+    _getch();
+        
+    while ((ch = _getch()) != 13) {
+        if (pos > 0 && ch == 'w') {
+      // Move up
+            pos--;
+        } else if (pos < size - 1 && ch == 's') {
+      // Move down
+            pos++;
+        } else if (ch == 'q') {
+      // Close
+            pos = -1;
+            break;
+        }
+
+        clearScreen();
+        updateMenu(arr, size, pos);
+    }
+
+    #else
+    getchar();
+    
+    while ((ch = getchar()) != '\n') {
+        if (pos > 0 && ch == 'w') {
+            pos--;
+        } else if (pos < size - 1 && ch == 's') {
+            pos++;
+        } else if (ch == 'q') {
+            pos = -1;
+            break;
+        }
+
+        clearScreen();
+        updateMenu(arr, size, pos);
+    }
+
+    #endif
+
+    #ifdef _WIN32
+    #else
+    system("stty icanon");
+    #endif
+
+    return pos + 1;
+}
+// This function would later serve as the main root from which all child menus spawn
+
+// Prints the Matrix
+void printMatr(int r, int c, int mat[r][c], int px, int py) { // Px and Py indicate where [-] should be
+  clearScreen();
+  int size = r * c * 4 + 1; // Calculates the Size
+  char matrix[size]; // Making the string to store the matrix in a string form
+  matrix[0] = '\0'; // Makes sure it is empty
+
+  for (int i = 0; i < r; i++) {
+    for (int j = 0; j < c; j++) {
+
+      char part[100] = ""; // Part stores the text/num to be concatenated
+      if (i == px && j == py) {
+        strcpy(part, "[-] ");
+        // printf("[-] ");
+      } else if (i < px || (i == px && j <= py)) {
+        // printf("%d ", mat[i][j]);
+        sprintf(part, "%d ", mat[i][j]); // The number is stored in a string form 
+        // strcat(part, num);
+      } else {
+        // printf(" - ");
+        strcpy(part, "- ");
+      }
+      if (j == (c - 1)) {
+        strcat(part, "\n");
+      }
+      strcat(matrix, part); // The Part is concatenated into the matrix
+    }
+  }
+  printCenteredText(matrix);
+}
+
+void inputMatric(int r, int c, int mat[r][c]) {
+
+  // Inputs the matrix, Printing eachtime
+  for (int i = 0; i < r; i++) {
+    for (int j = 0; j < c; j++) {
+      printMatr(r, c, mat, i, j);
+      scanf("%d", &mat[i][j]);
+    }
+  }
+  printMatr(r, c, mat, r, c);
+}
+
 /*
  *
  *@done Upper Triangle Matrix Identification
@@ -21,9 +242,9 @@
 
 int det_2x2()
 {
+	int det,ad,bc;
   int a[2][2];
   inputMatric(2,2,a);
-	int det,ad,bc;
 	ad = a[0][0] * a[1][1];
 	bc = a[0][1] * a[1][0];
 	det = ad-bc;
@@ -892,4 +1113,297 @@ int** read_from_file(char *fname, int x, int **ans, int *r, int *c){
   }
   fclose(fptr);
   return ans;
+}
+
+
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+// #include "headers/functions.h"
+// #include "headers/ui.h"
+// <<<<<<< Updated upstream
+// =======
+
+// #include <math.h>
+// >>>>>>> Stashed changes
+
+// <--------- Matrices Menu --------->
+
+void detM(){
+  char menuArr[4][100] = {"1. Determinant3x3","2. Determinant2x2","3. Adjoint3x3", "4. Adjoint2x2"};
+  int n;
+  n = selectMenu(menuArr,4);
+  
+  switch(n){
+    case 1:
+      det_3x3();
+      break;
+    case 2:
+      det_2x2();
+      break;
+    case 3:
+      adj_3x3();
+      break;
+    case 4:
+      adj_2x2();
+      break;
+    default:
+      printCenteredText("Invalid Input");
+      break;
+  }
+
+}
+
+void matM() {
+  const char *menu =
+      "1. Upper Triangular\n2. Lower Triangular\n3. Determinant\n4. "
+      "Transpose\n5. Scalar Multiplication\n6. Unit Matrix\n7. Null Matrix\n8. "
+      "Diagonal\n9. Idempotent\n10. Involuntary\n11. Nilpotent\n12. "
+      "Symmetric\n13. Skewsymmetric\n";
+  char menuArr[15][100] = {"1. Upper Triangular",
+                           "2. Lower Triangular",
+                           "3. Determinant/Adjoint",
+                           "4. Transpose",
+                           "5. Scalar Multiplication",
+                           "6. Unit Matrix",
+                           "7. Null Matrix",
+                           "8. Diagonal",
+                           "9. Idempotent",
+                           "10. Involuntary",
+                           "11. Nilpotent",
+                           "12. Symmetric",
+                           "13. Skewsymmetric",
+                           "14. Addition",
+                           "15. Subtraction"};
+  // printCenteredText(menu);
+  int n;
+  n = selectMenu(menuArr, 15);
+  // scanf("%d", &n);
+  switch (n) {
+  case 1:
+    if (u_tm()) {
+      printCenteredText("Is an upper triangular matrix");
+    } else {
+      printCenteredText("Is not an upper triangular matrix");
+    }
+    break;
+  case 2:
+    if (l_tm()) {
+      printCenteredText("Is an lower triangular matrix");
+    } else {
+      printCenteredText("Is not an lower triangular matrix");
+    }
+    break;
+  case 3:
+    // Determinent
+    // det_3x3();
+    detM();
+    break;
+  case 4:
+    transpose();
+
+    break;
+  case 5:
+    scalar_multiplication();
+    // Matrices
+    break;
+  case 6:
+    // Exit
+    if (unit_matrix()) {
+      printCenteredText("It is Unit Matrix");
+    } else {
+      printCenteredText("It is not Unit Matrix or Input was incorrect");
+    }
+    break;
+  case 7:
+    if (null_matrix()) {
+      printCenteredText("It is a null matrix");
+    } else {
+      printCenteredText("It is not a null matrix");
+    }
+    break;
+  case 8:
+    if (diag()) {
+      printCenteredText("It is a diagonal matrix");
+    } else {
+      printCenteredText("It is not a diagonal matrix");
+    };
+    break;
+  case 9:
+    idemponent();
+    break;
+  case 10:
+    invoulantry();
+    break;
+  case 11:
+    nilponent();
+    break;
+  case 12:
+    symmetric();
+    break;
+  case 13:
+    skewsym();
+    break;
+  case 14:
+    addM();
+    break;
+  case 15:
+    subM();
+    break;
+  }
+}
+
+
+// <--------- Arithmetic Menu --------->
+
+void arithM() {
+  const char *menu = "1. Addition\n2. Subtraction\n3. Multiplication\n4. "
+                     "Division\n5. Quadratic";
+  char menuArr[][100] = {"1. Addition", "2. Subtraction", "3. Multiplication",
+                         "4. Division", "5. Quadratic"};
+  // printCenteredText(menu);
+  int n = selectMenu(menuArr, 5);
+  // scanf("%d", &n);
+  switch (n) {
+  case 1:
+    add();
+    break;
+  case 2:
+    sub();
+    break;
+  case 3:
+    multiply();
+    break;
+  case 4:
+    division();
+    break;
+  case 5:
+    quad();
+    break;
+  }
+}
+// <<<<<<< Updated upstream
+
+// <--------- Trigonometry Menu --------->
+
+void trig() {
+
+  char menuarr[][100] = {"1. Sin",         "2. Cos",         "3. Tan",
+                         "4. Sec",         "5. Cosec",       "6. Cot",
+                         "7. Sin inverse", "8. Cos inverse", "9. Cot inverse"};
+
+  int n = selectMenu(menuarr, 9);
+
+  switch (n) {
+  case 1:
+    sine();
+    break;
+  case 2:
+    cosine();
+    break;
+  case 3:
+    tangent();
+    break;
+  case 4:
+    secant();
+    break;
+  case 5:
+    cosecant();
+    break;
+  case 6:
+    cotangent();
+    break;
+  case 7:
+    asine();
+    break;
+  case 8:
+    acosine();
+    break;
+  case 9:
+    atangent();
+    break;
+  default:
+    printCenteredText("invalid option");
+    break;
+  }
+}
+
+// <----- Log Menu ----->
+
+void logM() {
+
+  char menuarr[][100] = {"1. log", "2. ln"};
+
+  int n = selectMenu(menuarr, 2);
+
+  switch (n) {
+  case 1:
+    log_10();
+    break;
+  case 2:
+    ln();
+    break;
+  default:
+    printCenteredText("invalid option");
+    break;
+  }
+}
+
+
+// <------- Main Menu ------->
+
+void mainM() {
+  const char *asciiArt =
+      "    ___  ___      _        _        _____       _            _       _  "
+      "           \n"
+      "    |  \\/  |     | |      (_)      /  __ \\     | |          | |     | "
+      "|            \n"
+      "    | .  . | __ _| |_ _ __ ___  __ | /  \\/ __ _| | ___ _   _| | __ _| "
+      "|_ ___  _ __ \n"
+      "    | |\\/| |/ _` | __| '__| \\ \\/ / | |    / _` | |/ __| | | | |/ _` "
+      "| __/ _ \\| '__|\n"
+      "    | |  | | (_| | |_| |  | |>  <  | \\__/\\ (_| | | (__| |_| | | (_| | "
+      "|| (_) | |   \n"
+      "    \\_|  |_|\\__,_|\\__|_|  |_/_/\\_\\  "
+      "\\____/\\__,_|_|\\___|\\__,_|_|\\__,_|\\__\\___/|_|   \n";
+
+  printCenteredText(asciiArt);
+  system("sleep 1");
+  const char *menu = "1.Arithmetic\n2.Logarithmic\n3."
+                     "Trigonometric\n4.Matrices\n5.Exit\n";
+
+  char menuArr[][100] = {"1. Arithmetic", "2. Logarithmic", "3. Trigonometric",
+                         "4. Matrices", "5. Exit"};
+
+  int n = selectMenu(menuArr, 5);
+  switch (n) {
+  case 1:
+    // Arithmetic Menu
+    arithM();
+    break;
+  case 2:
+    // Logarithmic
+    logM();
+    break;
+  case 3:
+    // Trignometric
+    trig();
+    break;
+  case 4:
+    // Matrices
+    matM();
+    break;
+  case 5:
+  default:
+    // Exit
+    printf("Exiting...\n");
+    break;
+  }
+}
+
+int main() {
+  clearScreen();
+  mainM();
+  return 0;
 }
